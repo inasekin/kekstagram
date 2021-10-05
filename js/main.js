4033e1688623b824febeb789df04bbb76c3c3c9d
Ivan Nasekin <nasekin@ipavlov.ai>
2021-10-06 00:00:21 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно (MDN web docs)
 function getRandomInt(min, max) {
-  if (min >= 0 && max >= 0) {
-    if (max <= min) {
-      return false;
-    } else {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
+  if (min >= 0 && max >= 0 && max >= min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
   } else {
     return false;
   }
 }
+
 
 //Функция для проверки максимальной длины строки
 function checkMaxStringLength(checkedString, maxLength) {
@@ -19,5 +16,5 @@ function checkMaxStringLength(checkedString, maxLength) {
 }
 
 //Инициализация функций
-getRandomInt(0, 5);
+console.log(getRandomInt(0, 2));
 checkMaxStringLength('Какой-то тестовый комментарий', 140);
