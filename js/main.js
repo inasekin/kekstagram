29b5fbf89f777ecb254a39d56360671a80ab7f0b
Ivan Nasekin <nasekin@ipavlov.ai>
2021-10-15 20:42:22 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+const descriptions = [
+  'Весело',
+  'Осень',
+  'На учебе',
+  'скучно',
+];
+const names = [
+  'Иван',
+  'Марк',
+  'Евгений',
+  'Мария',
+  'Ангелина',
+  'Илья',
+  'Вера',
+];
+const textComments = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const MIN_DESCRIPTION_IDENTIFIER = 1;
+const MAX_DESCRIPTION_IDENTIFIER = 25;
+
+const MIN_NUMBER_OF_LIKES = 15;
+const MAX_NUMBER_OF_LIKES = 200;
+
+const MIN_NUMBER_FOR_LIKES = 1;
+const MAX_NUMBER_FOR_LIKES = 6;
 
 function getRandomInt(min, max) {
   if (min >= 0 && max >= 0 && max >= min) {
@@ -13,62 +45,22 @@ function checkMaxStringLength(checkedString, maxLength) {
   return checkedString.length <= maxLength;
 }
 
-getRandomInt(10, 0);
 checkMaxStringLength('Какой-то тестовый комментарий', 140);
 
-const description = [
-  'Весело',
-  'Осень',
-  'На учебе',
-  'скучно',
-];
-
-const names = [
-  'Иван',
-  'Марк',
-  'Евгений',
-  'Мария',
-  'Ангелина',
-  'Илья',
-  'Вера',
-];
-
-const textComments = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-
-const getArrayPart = (array) => array[getRandomInt(0, array.length - 1)];
-
-const getRandomMessage = () => getArrayPart(textComments);
-
-const getRandomName = () => getArrayPart(names);
-
-const getRandomIndex = () => getRandomInt(1, 25);
-
 const getRandomComment = () => ({
-  id: getRandomIndex(),
-  avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
-  message: getRandomMessage(),
-  name: getRandomName(),
+  id: getRandomInt(MIN_DESCRIPTION_IDENTIFIER, MAX_DESCRIPTION_IDENTIFIER),
+  avatar: `img/avatar-${getRandomInt(MIN_NUMBER_FOR_LIKES, MAX_NUMBER_FOR_LIKES)}.svg`,
+  message: textComments[getRandomInt(0, textComments.length - 1)],
+  name: names[getRandomInt(0, names.length - 1)],
 });
-
-const getRandomComments = () => Array.from({length: getRandomInt(1, 3)}, (value, index) => getRandomComment(index));
-
-const getRandomLikes = () => getRandomInt(15, 200);
-
-const getRandomDescription = () => getArrayPart(description);
-
+const getRandomComments = () => Array.from({length: getRandomInt(MIN_NUMBER_FOR_LIKES, MAX_NUMBER_FOR_LIKES)}, () => getRandomComment());
 const getRandomPhoto = () => ({
-  id: getRandomIndex(), // случайное число от 1-25 (index)
-  url: `photos/${getRandomInt(1, 25)}.jpg`,
-  description: getRandomDescription(),
-  likes: getRandomLikes(),
+  id: getRandomInt(MIN_DESCRIPTION_IDENTIFIER, MAX_DESCRIPTION_IDENTIFIER),
+  url: `photos/${getRandomInt(MIN_DESCRIPTION_IDENTIFIER, MAX_DESCRIPTION_IDENTIFIER)}.jpg`,
+  descriptions: descriptions[getRandomInt(0, descriptions.length - 1)],
+  likes: getRandomInt(MIN_NUMBER_OF_LIKES, MAX_NUMBER_OF_LIKES),
   comments: getRandomComments(),
 });
 
+getRandomComment();
 getRandomPhoto();
