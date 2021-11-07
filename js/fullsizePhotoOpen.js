41683daec24f589b68372336100c0933880119d0
Ivan Nasekin <nasekin@ipavlov.ai>
2021-11-07 19:49:55 +0300

    finish task2 of module 8

--- js/fullsizePhotoOpen.js
+++ js/fullsizePhotoOpen.js
@@ -4,6 +4,7 @@ const fullSizePictureModal = document.querySelector('.big-picture');
 const fullSizePictureModalImg = fullSizePictureModal.querySelector('.big-picture__img img');
 const likesCount = fullSizePictureModal.querySelector('.likes-count');
 const commentsCount = fullSizePictureModal.querySelector('.comments-count');
+const commentCounterViews = fullSizePictureModal.querySelector('.comments-count-for-viewing');
 const bodyNode = document.getElementsByTagName('body')[0];
 const closePictureBtn = document.getElementById('picture-cancel');
 const socialComments = fullSizePictureModal.querySelector('.social__comments');
@@ -15,6 +16,7 @@ const openFullSizePictureModal = () => {
   bodyNode.classList.add('modal-open');
 };
 const closeFullSizePictureModal = () => {
+  commentCounterViews.textContent = '5';
   fullSizePictureModal.classList.add('hidden');
   bodyNode.classList.remove('modal-open');
   document.removeEventListener('click', closeFullSizePictureModal);
@@ -51,46 +53,33 @@ const generateComments = (comments) => {
 };
 
 export const renderFullSizePictureModal = (picture) => {
+  socialComments.textContent = '';
   openFullSizePictureModal();
   fullSizePictureModalImg.src = picture.url;
   likesCount.textContent = picture.likes;
   commentsCount.textContent = picture.comments.length;
-
-  const commentCounterViews = document.querySelector('.comments-count-for-viewing');
   const commentCounter = document.querySelector('.social__comment-count');
   const btnLoadNewComments = document.querySelector('.comments-loader');
-  const currentCommentCount = 5;
+  let currentCommentCount = 5;
   if (picture.comments.length > currentCommentCount) {
     btnLoadNewComments.classList.remove('hidden');
     const currentComments = picture.comments.slice(0, currentCommentCount);
-    console.log(currentComments);
-    socialComments.textContent = '';
     socialComments.appendChild(generateComments(currentComments));
     btnLoadNewComments.addEventListener('click', () => {
-      const nextCommentCount = 5 + currentComments;
-      const nextComments = picture.comments.slice(currentComments, nextCommentCount);
+      const nextCommentCount = 5 + currentCommentCount;
+      const nextComments = picture.comments.slice(currentCommentCount, nextCommentCount);
+      currentCommentCount = nextCommentCount;
+      commentCounterViews.textContent = currentCommentCount;
+      socialComments.appendChild(generateComments(nextComments));
+      if (picture.comments.length <= currentCommentCount) {
+        btnLoadNewComments.classList.add('hidden');
+      }
     });
   } else {
+    commentCounter.classList.add('hidden');
     btnLoadNewComments.classList.add('hidden');
+    socialComments.appendChild(generateComments(picture.comments));
   }
-
-  // btnLoadNewComments.addEventListener('click', () => {
-  //   picture.comments.length = Number(commentsCount.textContent);
-  //   console.log(picture.comments);
-  //   socialComments.appendChild(generateComments(picture.comments));
-  // });
-  // if (picture.comments.length > 5) {
-  //   btnLoadNewComments.classList.remove('hidden');
-  //   socialComments.innerHTML = '';
-  //   picture.comments.length = 5;
-  //   commentCounterViews.textContent = 5;
-  //   socialComments.appendChild(generateComments(picture.comments));
-  // } else {
-  //   socialComments.innerHTML = '';
-  //   btnLoadNewComments.classList.add('hidden');
-  //   socialComments.appendChild(generateComments(picture.comments));
-  //   commentCounterViews.textContent = picture.comments.length;
-  // }
   socialCaption.textContent = picture.description;
   document.addEventListener('keydown', onCloseEscapeKeydown);
 };
