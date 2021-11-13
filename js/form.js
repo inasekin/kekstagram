24642ce1cdb18831e52172c953a8590500ca5ab8
Ivan Nasekin <nasekin@ipavlov.ai>
2021-11-13 19:39:33 +0300

    add changes for mentor comments

--- js/form.js
+++ js/form.js
@@ -14,12 +14,11 @@ const imageUploadCancel = document.querySelector('.img-upload__cancel');
 const textHashtags = document.querySelector('.text__hashtags');
 const textComment = document.querySelector('.text__description');
 
-const errorProcessing = () => {
+const enabledHashtagError = () => {
   textHashtags.classList.add('error-input');
 };
 
 const hashtagValidationHandler = (evt) => {
-  if (evt.target.closest('input[class="text__hashtags"]')) {
     evt.target.value = evt.target.value.replace(/\s+/g, ' ');
     const listOfHashtags = evt.target.value.toLowerCase().split(' ');
 
@@ -28,31 +27,31 @@ const hashtagValidationHandler = (evt) => {
         case listOfHashtags[0] === '':
           textHashtags.value = textHashtags.value.trim();
           textHashtags.setCustomValidity('');
-          errorProcessing();
+          enabledHashtagError();
           break;
         case !hashtag.startsWith('#'):
           textHashtags.setCustomValidity('хеш-тег должен начинаться с решётки #');
-          errorProcessing();
+          enabledHashtagError();
           break;
         case hashtag === '#':
           textHashtags.setCustomValidity('хеш-тег не может состоять только из одной решётки #');
-          errorProcessing();
+          enabledHashtagError();
           break;
         case hashtag.length > MAX_HASHTAG_LENGTH:
           textHashtags.setCustomValidity('максимальная длина одного хэш-тега 20 символов, включая решётку #');
-          errorProcessing();
+          enabledHashtagError();
           break;
         case !REGULAR_EXPRESSION_FOR_HASHTAGS.test(hashtag):
           textHashtags.setCustomValidity('хеш-тег не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.;');
-          errorProcessing();
+          enabledHashtagError();
           break;
         case listOfHashtags.length > MAX_HASHTAG_ARRAY_LENGTH:
           textHashtags.setCustomValidity('нельзя указать больше пяти хэш-тегов');
-          errorProcessing();
+          enabledHashtagError();
           break;
         case checkArrayDuplicates(listOfHashtags):
           textHashtags.setCustomValidity('один и тот же хэш-тег не может быть использован дважды');
-          errorProcessing();
+          enabledHashtagError();
           break;
         default:
           textHashtags.classList.remove('error-input');
@@ -60,11 +59,9 @@ const hashtagValidationHandler = (evt) => {
     }
 
     return evt.target.reportValidity();
-  }
 };
 
 const commentsValidationHandler = (evt) => {
-  if (evt.target.closest('textarea[class="text__description"]')) {
     if (!checkMaxStringLength(textComment.value, MAX_COMMENT_LENGTH)) {
       evt.target.setCustomValidity(' Длина комментария не может составлять больше 140 символов');
       evt.target.classList.add('error-input');
@@ -74,7 +71,6 @@ const commentsValidationHandler = (evt) => {
     }
 
     return evt.target.reportValidity();
-  }
 };
 
 textHashtags.addEventListener('change', hashtagValidationHandler);
