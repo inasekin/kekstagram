cba0d3098b58435e8206609b20b0a074cd7e5a4e
Ivan Nasekin <nasekin@ipavlov.ai>
2021-11-14 22:55:17 +0300

    refactor scale.js

--- js/data.js
+++ js/data.js
@@ -14,6 +14,11 @@ export const MAX_HASHTAG_ARRAY_LENGTH = 5;
 export const SCALE_STEP = 25;
 export const SCALE_STEP_HIDDEN = 0.25;
 
+export const DEFAULT_SCALE_VALUE = '100%';
+export const DEFAULT_SCALE_HIDDEN_VALUE = '1';
+export const REDUCE_SCALE_VALUE = '0.25';
+export const VALUE_OF_DECIMAL_NUMBER_SYSTEM = 10;
+
 export const REGULAR_EXPRESSION_FOR_HASHTAGS = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 
 export const DESCRIPTIONS = [
--- js/scale.js
+++ js/scale.js
@@ -1,5 +1,11 @@
-import {SCALE_STEP_HIDDEN} from './data.js';
-import {SCALE_STEP} from './data.js';
+import {
+  SCALE_STEP_HIDDEN,
+  SCALE_STEP,
+  DEFAULT_SCALE_VALUE,
+  DEFAULT_SCALE_HIDDEN_VALUE,
+  REDUCE_SCALE_VALUE,
+  VALUE_OF_DECIMAL_NUMBER_SYSTEM
+} from './data.js';
 
 export const formImg = document.querySelector('.img-upload__preview img');
 
@@ -9,24 +15,23 @@ const scaleMinus = document.querySelector('.scale__control--smaller');
 const scaleValue = document.querySelector('.scale__control--value');
 const scaleHidden = document.querySelector('#hidden-scale');
 
-
 export const setDefaultScale = () => {
-  scaleValue.value = '100%';
-  scaleHidden.value = '1';
+  scaleValue.value = DEFAULT_SCALE_VALUE;
+  scaleHidden.value = DEFAULT_SCALE_HIDDEN_VALUE;
   formImg.style.transform = `scale(${scaleHidden.value})`;
 };
 
 const reduceScale = () => {
-  if (scaleHidden.value !== '0.25') {
-    scaleValue.value = `${parseInt(scaleValue.value, 10) - SCALE_STEP}%`;
+  if (scaleHidden.value !== REDUCE_SCALE_VALUE) {
+    scaleValue.value = `${parseInt(scaleValue.value, VALUE_OF_DECIMAL_NUMBER_SYSTEM) - SCALE_STEP}%`;
     scaleHidden.value  = `${Number(scaleHidden.value) - SCALE_STEP_HIDDEN}`;
     formImg.style.transform = `scale(${scaleHidden.value})`;
   }
 };
 
 const addScale = () => {
-  if (scaleHidden.value !== '1') {
-    scaleValue.value = `${parseInt(scaleValue.value, 10) + SCALE_STEP}%`;
+  if (scaleHidden.value !== DEFAULT_SCALE_HIDDEN_VALUE) {
+    scaleValue.value = `${parseInt(scaleValue.value, VALUE_OF_DECIMAL_NUMBER_SYSTEM) + SCALE_STEP}%`;
     scaleHidden.value = `${Number(scaleHidden.value) + SCALE_STEP_HIDDEN}`;
     formImg.style.transform = `scale(${scaleHidden.value})`;
   }
